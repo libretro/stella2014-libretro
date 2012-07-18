a2e80b310cde04cd5a1e8d2565a3bf44fcefe506
clobber <brymaster@gmail.com>
2012-07-17 21:49:27 -0500

    Remove mednafen code

--- Makefile
+++ Makefile
@@ -35,11 +35,9 @@ else
    ENDIANNESS_DEFINES := -DLSB_FIRST
    FLAGS += -DHAVE__MKDIR
 endif
-MEDNAFEN_DIR := mednafen
-STELLA_DIR := $(MEDNAFEN_DIR)/stella
+STELLA_DIR := stella
 
-STELLA_SOURCES := $(STELLA_DIR)/stella.cpp	\
-	$(STELLA_DIR)/Console.cpp \
+STELLA_SOURCES := $(STELLA_DIR)/Console.cpp \
 	$(STELLA_DIR)/Settings.cpp \
 	$(STELLA_DIR)/SoundSDL.cpp \
 	$(STELLA_DIR)/cart/Cart0840.cpp \
@@ -96,77 +94,10 @@ STELLA_SOURCES := $(STELLA_DIR)/stella.cpp	\
 	$(STELLA_DIR)/utility/Serializer.cpp \
 	$(STELLA_DIR)/properties/Props.cpp \
 	$(STELLA_DIR)/properties/PropsSet.cpp
-	
-	
 
-MEDNAFEN_SOURCES := $(MEDNAFEN_DIR)/cdrom/cdromif.cpp \
-	$(MEDNAFEN_DIR)/mednafen.cpp \
-	$(MEDNAFEN_DIR)/PSFLoader.cpp \
-	$(MEDNAFEN_DIR)/error.cpp \
-	$(MEDNAFEN_DIR)/math_ops.cpp \
-	$(MEDNAFEN_DIR)/settings.cpp \
-	$(MEDNAFEN_DIR)/netplay.cpp \
-	$(MEDNAFEN_DIR)/general.cpp \
-	$(MEDNAFEN_DIR)/player.cpp \
-	$(MEDNAFEN_DIR)/cdplay.cpp \
-	$(MEDNAFEN_DIR)/FileWrapper.cpp \
-	$(MEDNAFEN_DIR)/state.cpp \
-	$(MEDNAFEN_DIR)/tests.cpp \
-	$(MEDNAFEN_DIR)/movie.cpp \
-	$(MEDNAFEN_DIR)/endian.cpp \
-	$(MEDNAFEN_DIR)/qtrecord.cpp \
-	$(MEDNAFEN_DIR)/cdrom/CDAccess.cpp \
-	$(MEDNAFEN_DIR)/cdrom/CDAccess_Image.cpp \
-	$(MEDNAFEN_DIR)/cdrom/CDUtility.cpp \
-	$(MEDNAFEN_DIR)/cdrom/lec.cpp \
-	$(MEDNAFEN_DIR)/cdrom/SimpleFIFO.cpp \
-	$(MEDNAFEN_DIR)/cdrom/audioreader.cpp \
-	$(MEDNAFEN_DIR)/cdrom/galois.cpp \
-	$(MEDNAFEN_DIR)/cdrom/pcecd.cpp \
-	$(MEDNAFEN_DIR)/cdrom/scsicd.cpp \
-	$(MEDNAFEN_DIR)/cdrom/recover-raw.cpp \
-	$(MEDNAFEN_DIR)/cdrom/l-ec.cpp \
-	$(MEDNAFEN_DIR)/cdrom/crc32.cpp \
-	$(MEDNAFEN_DIR)/memory.cpp \
-	$(MEDNAFEN_DIR)/mempatcher.cpp \
-	$(MEDNAFEN_DIR)/video/video.cpp \
-	$(MEDNAFEN_DIR)/video/text.cpp \
-	$(MEDNAFEN_DIR)/video/font-data.cpp \
-	$(MEDNAFEN_DIR)/video/tblur.cpp \
-	$(MEDNAFEN_DIR)/video/png.cpp \
-	$(MEDNAFEN_DIR)/video/Deinterlacer.cpp \
-	$(MEDNAFEN_DIR)/video/surface.cpp \
-	$(MEDNAFEN_DIR)/video/resize.cpp \
-	$(MEDNAFEN_DIR)/string/escape.cpp \
-	$(MEDNAFEN_DIR)/string/ConvertUTF.cpp \
-	$(MEDNAFEN_DIR)/sound/Blip_Buffer.cpp \
-	$(MEDNAFEN_DIR)/sound/Fir_Resampler.cpp \
-	$(MEDNAFEN_DIR)/sound/Stereo_Buffer.cpp \
-	$(MEDNAFEN_DIR)/sound/WAVRecord.cpp \
-	$(MEDNAFEN_DIR)/sound/sound.cpp \
-	$(MEDNAFEN_DIR)/file.cpp \
-	$(MEDNAFEN_DIR)/okiadpcm.cpp \
-	$(MEDNAFEN_DIR)/md5.cpp
+LIBRETRO_SOURCES := libretro.cpp
 
-MPC_SRC := $(wildcard $(MEDNAFEN_DIR)/mpcdec/*.c)
-TREMOR_SRC := $(wildcard $(MEDNAFEN_DIR)/tremor/*.c)
-
-SOURCES_C := $(MEDNAFEN_DIR)/trio/trio.c \
-	$(MPC_SRC) \
-	$(TREMOR_SRC) \
-	$(MEDNAFEN_DIR)/trio/trionan.c \
-	$(MEDNAFEN_DIR)/trio/triostr.c \
-	$(MEDNAFEN_DIR)/string/world_strtod.c \
-	$(MEDNAFEN_DIR)/compress/blz.c \
-	$(MEDNAFEN_DIR)/compress/unzip.c \
-	$(MEDNAFEN_DIR)/compress/minilzo.c \
-	$(MEDNAFEN_DIR)/compress/quicklz.c \
-	$(MEDNAFEN_DIR)/compress/ioapi.c \
-	$(MEDNAFEN_DIR)/resampler/resample.c
-
-LIBRETRO_SOURCES := libretro.cpp stubs.cpp thread.cpp
-
-SOURCES := $(LIBRETRO_SOURCES) $(HW_CPU_SOURCES) $(HW_MISC_SOURCES) $(HW_SOUND_SOURCES) $(HW_VIDEO_SOURCES) $(STELLA_SOURCES) $(MEDNAFEN_SOURCES)
+SOURCES := $(LIBRETRO_SOURCES) $(STELLA_SOURCES)
 OBJECTS := $(SOURCES:.cpp=.o) $(SOURCES_C:.c=.o)
 
 all: $(TARGET)
@@ -179,7 +110,7 @@ endif
 
 LDFLAGS += $(fpic) -lz $(SHARED)
 FLAGS += -msse -msse2 -Wall $(fpic) -fno-strict-overflow
-FLAGS += -I. -Imednafen -Imednafen/include -Imednafen/intl -Imednafen/hw_cpu -Imednafen/hw_misc -Imednafen/hw_sound -Imednafen/hw_video -pthread -Imednafen/stella -Imednafen/stella/cart -Imednafen/stella/input -Imednafen/stella/system -Imednafen/stella/utility -Imednafen/stella/properties
+FLAGS += -I. -pthread -Istella -Istella/cart -Istella/input -Istella/system -Istella/utility -Istella/properties
 
 WARNINGS := -Wall \
 	-Wno-narrowing \
@@ -192,9 +123,7 @@ WARNINGS := -Wall \
 	-Wno-strict-aliasing \
 	-Wno-overflow
 
-FLAGS += -DLSB_FIRST -DHAVE_MKDIR -DSIZEOF_DOUBLE=8 $(WARNINGS) \
-			-DMEDNAFEN_VERSION=\"0.9.22\" -DMEDNAFEN_VERSION_NUMERIC=922 -DPSS_STYLE=1 -DMPC_FIXED_POINT -DARCH_X86 \
-			-DWANT_STELLA_EMU -DSTDC_HEADERS
+FLAGS += -DLSB_FIRST -DHAVE_MKDIR -DSIZEOF_DOUBLE=8 $(WARNINGS)
 
 CXXFLAGS += $(FLAGS) -DHAVE_INTTYPES -DKeyboard=StellaKeyboard
 CFLAGS += $(FLAGS) -std=gnu99
--- libretro.cpp
+++ libretro.cpp
@@ -1,299 +1,292 @@
-#include "mednafen/mednafen-types.h"
-#include "mednafen/mednafen.h"
-#include "mednafen/git.h"
-#include "mednafen/general.h"
-#include <iostream>
-#include "libretro.h"
-
-static MDFNGI *game;
-static retro_video_refresh_t video_cb;
-static retro_audio_sample_t audio_cb;
-static retro_audio_sample_batch_t audio_batch_cb;
-static retro_environment_t environ_cb;
-static retro_input_poll_t input_poll_cb;
-static retro_input_state_t input_state_cb;
-
-static MDFN_Surface *surf;
-
-static uint16_t conv_buf[160 * 210] __attribute__((aligned(16)));
-static uint32_t mednafen_buf[160 * 210] __attribute__((aligned(16)));
-static bool failed_init;
-
-void retro_init()
-{
-    MDFN_PixelFormat pix_fmt(MDFN_COLORSPACE_RGB, 16, 8, 0, 24);
-    surf = new MDFN_Surface(mednafen_buf, 160, 210, 160, pix_fmt);
-    
-    std::vector<MDFNGI*> ext;
-    MDFNI_InitializeModules(ext);
-    
-    
-    const char *dir = NULL;
-    const char *saves = NULL;
-    
-    std::vector<MDFNSetting> settings;
-    
-    environ_cb(RETRO_ENVIRONMENT_GET_SAVES_DIRECTORY, &saves);
-    if (environ_cb(RETRO_ENVIRONMENT_GET_SYSTEM_DIRECTORY, &dir) && dir)
-    {
-        std::string save_path = saves;
-        
-        MDFNSetting stella_ramrandom = { "stella.ramrandom", MDFNSF_NOFLAGS, "Use random values to initialize RAM", NULL, MDFNST_BOOL, "1"};
-        MDFNSetting stella_fastscbios = { "stella.fastscbios", MDFNSF_NOFLAGS, "Use quick method to init SuperCharger BIOS", NULL, MDFNST_BOOL, "0"};
-        MDFNSetting stella_colorloss = { "stella.colorloss", MDFNSF_NOFLAGS, "Emulate Color loss", NULL, MDFNST_BOOL, "0"};
-        MDFNSetting stella_tiadriven = { "stella.tiadriven", MDFNSF_NOFLAGS, "Randomly toggle unused TIA pins", NULL, MDFNST_BOOL, "0"};
-        MDFNSetting stella_palette = { "stella.palette", MDFNSF_NOFLAGS, "Color Palette to use", NULL, MDFNST_STRING, "standard"};
-        MDFNSetting stella_framerate = { "stella.framerate", MDFNSF_NOFLAGS, "Unused", NULL, MDFNST_UINT, "60", "30", "70"};
-        MDFNSetting filesys = { "filesys.path_sav", MDFNSF_NOFLAGS, "Memcards", NULL, MDFNST_STRING, save_path.c_str() };
-        settings.push_back(stella_ramrandom);
-        settings.push_back(stella_fastscbios);
-        settings.push_back(stella_colorloss);
-        settings.push_back(stella_tiadriven);
-        settings.push_back(stella_palette);
-        settings.push_back(stella_framerate);
-        settings.push_back(filesys);
-        MDFNI_Initialize(dir, settings);
-    }
-    else
-    {
-        fprintf(stderr, "System directory is not defined. Cannot continue ...\n");
-        failed_init = true;
-    }
-    
-    // Hints that we need a fairly powerful system to run this.
-    unsigned level = 3;
-    environ_cb(RETRO_ENVIRONMENT_SET_PERFORMANCE_LEVEL, &level);
-}
-
-void retro_deinit()
-{
-   delete surf;
-   surf = NULL;
-}
-
-void retro_reset()
-{
-   MDFNI_Reset();
-}
-
-bool retro_load_game_special(unsigned, const struct retro_game_info *, size_t)
-{
-   return false;
-}
-
-bool retro_load_game(const struct retro_game_info *info)
-{
-    if (failed_init)
-        return false;
-    game = MDFNI_LoadGame("stella", info->path);
-    return game;
-}
-
-void retro_unload_game()
-{
-   MDFNI_CloseGame();
-}
-
-#ifndef __SSE2__
-#error "SSE2 required."
+#ifndef _MSC_VER
+#include <stdbool.h>
+#endif
+#include <sched.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef __CELLOS_LV2__
+//#include <malloc.h>
 #endif
 
-#include <emmintrin.h>
+#ifdef _MSC_VER
+#define snprintf _snprintf
+#pragma pack(1)
+#endif
 
-// PSX core should be able to output ARGB1555 directly,
-// so we can avoid this conversion step.
-// Done in SSE2 here because any system that can run this
-// core to begin with will be at least that powerful (as of writing).
-static inline void convert_surface()
+#include "libretro.h"
+
+#include "Settings.hxx"
+#include "TIA.hxx"
+#include "Props.hxx"
+#include "PropsSet.hxx"
+#include "Cart.hxx"
+#include "Console.hxx"
+#include "Serializer.hxx"
+#include "Event.hxx"
+#include "Switches.hxx"
+#include "MD5.hxx"
+#include "SoundSDL.hxx"
+
+//extern MDFNGI EmulatedStella;
+
+//static bool failed_init;
+struct					Stella
 {
-   const uint32_t *pix = surf->pixels;
-   for (unsigned i = 0; i < 160 * 210; i += 8)
-   {
-      __m128i pix0 = _mm_load_si128((const __m128i*)(pix + i + 0));
-      __m128i pix1 = _mm_load_si128((const __m128i*)(pix + i + 4));
-
-      __m128i red0   = _mm_and_si128(pix0, _mm_set1_epi32(0xf80000));
-      __m128i green0 = _mm_and_si128(pix0, _mm_set1_epi32(0x00f800));
-      __m128i blue0  = _mm_and_si128(pix0, _mm_set1_epi32(0x0000f8));
-      __m128i red1   = _mm_and_si128(pix1, _mm_set1_epi32(0xf80000));
-      __m128i green1 = _mm_and_si128(pix1, _mm_set1_epi32(0x00f800));
-      __m128i blue1  = _mm_and_si128(pix1, _mm_set1_epi32(0x0000f8));
-
-      red0   = _mm_srli_epi32(red0,   19 - 10); 
-      green0 = _mm_srli_epi32(green0, 11 -  5); 
-      blue0  = _mm_srli_epi32(blue0,   3 -  0); 
-
-      red1   = _mm_srli_epi32(red1,   19 - 10); 
-      green1 = _mm_srli_epi32(green1, 11 -  5); 
-      blue1  = _mm_srli_epi32(blue1,   3 -  0); 
-
-      __m128i res0 = _mm_or_si128(_mm_or_si128(red0, green0), blue0);
-      __m128i res1 = _mm_or_si128(_mm_or_si128(red1, green1), blue1);
-
-      _mm_store_si128((__m128i*)(conv_buf + i), _mm_packs_epi32(res0, res1));
-   }
-}
-
-// Hardcoded for PSX. No reason to parse lots of structures ...
-// See mednafen/psx/input/gamepad.cpp
-static void update_input()
-{
-   static uint16_t input_buf[2];
-   input_buf[0] = input_buf[1] = 0;
-   static unsigned map[] = {
-      RETRO_DEVICE_ID_JOYPAD_Y,
-      RETRO_DEVICE_ID_JOYPAD_B,
-      RETRO_DEVICE_ID_JOYPAD_SELECT,
-      RETRO_DEVICE_ID_JOYPAD_START,
-      RETRO_DEVICE_ID_JOYPAD_UP,
-      RETRO_DEVICE_ID_JOYPAD_RIGHT,
-      RETRO_DEVICE_ID_JOYPAD_DOWN,
-      RETRO_DEVICE_ID_JOYPAD_LEFT,
-      RETRO_DEVICE_ID_JOYPAD_A,
-      RETRO_DEVICE_ID_JOYPAD_X,
-      RETRO_DEVICE_ID_JOYPAD_L,
-      RETRO_DEVICE_ID_JOYPAD_R,
-      RETRO_DEVICE_ID_JOYPAD_L2,
-   };
-
-   for (unsigned j = 0; j < 2; j++)
-   {
-      for (unsigned i = 0; i < 13; i++)
-         input_buf[j] |= map[i] != -1u &&
-            input_state_cb(j, RETRO_DEVICE_JOYPAD, 0, map[i]) ? (1 << i) : 0;
-   }
-
-   // Possible endian bug ...
-   game->SetInput(0, "gamepad", &input_buf[0]);
-   game->SetInput(1, "gamepad", &input_buf[1]);
-}
-
-void retro_run()
-{
-   input_poll_cb();
-
-   update_input();
-
-   static int16_t sound_buf[0x10000];
-   static MDFN_Rect rects[210];
-
-   EmulateSpecStruct spec = {0}; 
-   spec.surface = surf;
-   spec.SoundRate = 44100;
-   spec.SoundBuf = sound_buf;
-   spec.LineWidths = rects;
-   spec.SoundBufMaxSize = sizeof(sound_buf) / 2;
-   spec.SoundVolume = 1.0;
-   spec.soundmultiplier = 1.0;
-
-   rects[0].w = ~0;
-   MDFNI_Emulate(&spec);
+	Console*			GameConsole;
+	Settings			GameSettings;
     
-   //unsigned width = rects[0].w;
-   //unsigned height = spec.DisplayRect.h;
-   unsigned width = 160;
-   unsigned height = 210;
+	const uInt32*		Palette;
     
-   convert_surface();
-   video_cb(conv_buf, width, height, width << 1);
+	SoundSDL			Sound;
+    
+	Stella() 	{GameConsole = 0; Palette = 0;}
+	~Stella()	{delete GameConsole;}
+};
+Stella*				stella;
 
-   audio_batch_cb(spec.SoundBuf, spec.SoundBufSize);
+//Set the palette for the current stella instance
+void stellaMDFNSetPalette (const uInt32* palette)
+{
+	if(stella)
+	{
+		stella->Palette = palette;
+	}
 }
+
+//Get the settings from the current stella instance
+Settings& stellaMDFNSettings ()
+{
+	if(stella)
+	{
+		return stella->GameSettings;
+	}
+    
+	//HACK
+	abort();
+}
+
+static retro_video_refresh_t video_cb;
+static retro_input_poll_t input_poll_cb;
+static retro_input_state_t input_state_cb;
+static retro_environment_t environ_cb;
+static retro_audio_sample_batch_t audio_batch_cb;
+
+void retro_set_environment(retro_environment_t cb) { environ_cb = cb; }
+void retro_set_video_refresh(retro_video_refresh_t cb) { video_cb = cb; }
+void retro_set_audio_sample(retro_audio_sample_t cb) { (void)cb; }
+void retro_set_audio_sample_batch(retro_audio_sample_batch_t cb) { audio_batch_cb = cb; }
+void retro_set_input_poll(retro_input_poll_t cb) { input_poll_cb = cb; }
+void retro_set_input_state(retro_input_state_t cb) { input_state_cb = cb; }
+
+/************************************
+ * libretro implementation
+ ************************************/
+
+static struct retro_system_av_info g_av_info;
 
 void retro_get_system_info(struct retro_system_info *info)
 {
-   memset(info, 0, sizeof(*info));
-   info->library_name     = "Mednafen Stella";
-   info->library_version  = "0.9.22";
-   info->need_fullpath    = true;
-   info->valid_extensions = "a26|A26";
+    memset(info, 0, sizeof(*info));
+	info->library_name = "Stella";
+	info->library_version = "3.4.1";
+	info->need_fullpath = true;
+	info->valid_extensions = "a26|A26";
 }
 
 void retro_get_system_av_info(struct retro_system_av_info *info)
 {
-   memset(info, 0, sizeof(*info));
-   // Just assume NTSC for now. TODO: Verify FPS.
-   info->timing.fps            = 60;
-   info->timing.sample_rate    = 44100;
-   info->geometry.base_width   = 160;
-   info->geometry.base_height  = 210;
-   info->geometry.max_width    = 160;
-   info->geometry.max_height   = 210;
-   info->geometry.aspect_ratio = 4.0 / 3.0;
+    memset(info, 0, sizeof(*info));
+    // Just assume NTSC for now. TODO: Verify FPS.
+    info->timing.fps            = stella->GameConsole->getFramerate(); //60?
+    info->timing.sample_rate    = 44100;
+    info->geometry.base_width   = 160;
+    info->geometry.base_height  = 210;
+    info->geometry.max_width    = 160;
+    info->geometry.max_height   = 210;
+    info->geometry.aspect_ratio = 4.0 / 3.0;
 }
 
-unsigned retro_get_region(void)
+void retro_set_controller_port_device(unsigned port, unsigned device)
 {
-   return RETRO_REGION_NTSC;
+   (void)port;
+   (void)device;
 }
 
-unsigned retro_api_version(void)
-{
-   return RETRO_API_VERSION;
+size_t retro_serialize_size(void) 
+{ 
+	//return STATE_SIZE;
+	return 0;
 }
 
-// TODO: Allow for different kinds of joypads?
-void retro_set_controller_port_device(unsigned, unsigned)
-{}
-
-void retro_set_environment(retro_environment_t cb)
-{
-   environ_cb = cb;
+bool retro_serialize(void *data, size_t size)
+{ 
+   //if (size != STATE_SIZE)
+   //   return FALSE;
 }
 
-void retro_set_audio_sample(retro_audio_sample_t cb)
+bool retro_unserialize(const void *data, size_t size)
 {
-   audio_cb = cb;
-}
-
-void retro_set_audio_sample_batch(retro_audio_sample_batch_t cb)
-{
-   audio_batch_cb = cb;
-}
-
-void retro_set_input_poll(retro_input_poll_t cb)
-{
-   input_poll_cb = cb;
-}
-
-void retro_set_input_state(retro_input_state_t cb)
-{
-   input_state_cb = cb;
-}
-
-void retro_set_video_refresh(retro_video_refresh_t cb)
-{
-   video_cb = cb;
-}
-
-size_t retro_serialize_size(void)
-{
-   return 0;
-}
-
-bool retro_serialize(void *, size_t)
-{
-   return false;
-}
-
-bool retro_unserialize(const void *, size_t)
-{
-   return false;
-}
-
-void *retro_get_memory_data(unsigned)
-{
-   return NULL;
-}
-
-size_t retro_get_memory_size(unsigned)
-{
-   return 0;
+   //if (size != STATE_SIZE)
+   //   return FALSE;
 }
 
 void retro_cheat_reset(void)
 {}
 
-void retro_cheat_set(unsigned, bool, const char *)
-{}
+void retro_cheat_set(unsigned index, bool enabled, const char *code)
+{
+   (void)index;
+   (void)enabled;
+   (void)code;
+}
 
+bool retro_load_game(const struct retro_game_info *info)
+{
+    stella = new Stella();
+   const char *full_path;
+    
+   full_path = info->path;
+    
+	//Get the game properties
+    //string MD5(const uInt8* buffer, uInt32 length)
+    string cartMD5 = MD5((const uInt8*)info->data, info->size);
+    PropertiesSet propslist(0);
+    Properties gameProperties;
+    propslist.getMD5(cartMD5, gameProperties);
+    
+    //Load the cart
+    string cartType = gameProperties.get(Cartridge_Type);
+    string cartID = "";
+    //static Cartridge* create(const uInt8* image, uInt32 size, string& md5,
+    //                         string& dtype, string& id, Settings& settings);
+    Cartridge* stellaCart = Cartridge::create((const uInt8*)info->data, info->size, cartMD5, cartType, cartID, stella->GameSettings);
+    
+    //if(stellaCart == 0)
+    //{
+    //    printf("Stella: Failed to load cartridge.");
+    //    return false;
+    //}
+    
+    //Create the console
+    stella->GameConsole = new Console(stella->Sound, stellaCart, gameProperties);
+    
+    //Init sound
+    stella->Sound.open();
+    
+    //frame rate
+    //stella->GameConsole->getFramerate();
+    
+    //stella->Palette = 0; //NTSC
+    
+    /*
+     //INPUT
+     //Update stella's event structure
+     for(int i = 0; i != 2; i ++)
+     {
+     //Get the base event id for this port
+     Event::Type baseEvent = (i == 0) ? Event::JoystickZeroUp : Event::JoystickOneUp;
+     
+     //Get the input data for this port and stuff it in the event structure
+     uint32_t inputState = Input::GetPort<3>(i);
+     for(int j = 0; j != 19; j ++, inputState >>= 1)
+     {
+     stella->GameConsole->event().set((Event::Type)(baseEvent + j), inputState & 1);
+     }
+     }
+     
+     //Update the reset and select events
+     uint32_t inputState = Input::GetPort<0, 3>() >> 19;
+     stella->GameConsole->event().set(Event::ConsoleSelect, inputState & 1);
+     stella->GameConsole->event().set(Event::ConsoleReset, inputState & 2);
+     */
+    //Tell all input devices to read their state from the event structure
+    stella->GameConsole->switches().update();
+    stella->GameConsole->controller(Controller::Left).update();
+    stella->GameConsole->controller(Controller::Right).update();
+
+   return true;
+}
+
+bool retro_load_game_special(unsigned game_type, const struct retro_game_info *info, size_t num_info)
+{
+   (void)game_type;
+   (void)info;
+   (void)num_info;
+   return false;
+}
+
+void retro_unload_game(void) 
+{
+
+}
+
+unsigned retro_get_region(void)
+{
+    return RETRO_REGION_NTSC;
+}
+
+unsigned retro_api_version(void)
+{
+    return RETRO_API_VERSION;
+}
+
+void *retro_get_memory_data(unsigned id)
+{
+    return NULL;
+}
+
+size_t retro_get_memory_size(unsigned id)
+{
+    return 0;
+}
+
+void retro_init(void)
+{
+    unsigned level = 3;
+    environ_cb(RETRO_ENVIRONMENT_SET_PERFORMANCE_LEVEL, &level);
+}
+
+void retro_deinit(void)
+{
+    delete stella;
+    stella = 0;
+	//free(videoBuffer);
+}
+
+void retro_reset(void)
+{
+	stella->GameConsole->system().reset();
+}
+
+void retro_run(void) 
+{
+	/*
+     Int32 frameWidth = stella->GameConsole->tia().width();
+     Int32 frameHeight = stella->GameConsole->tia().height();
+     
+     for(int i = 0; i != frameHeight; i ++)
+     {
+     for(int j = 0; j != frameWidth; j ++)
+     {
+     espec->surface->pixels[i * espec->surface->pitchinpix + j] = stella->Palette[stella->GameConsole->tia().currentFrameBuffer()[i * frameWidth + j] & 0xFF];
+     }
+     }*/
+    
+    stella->GameConsole->tia().update();
+	
+	video_cb(stella->GameConsole->tia().currentFrameBuffer(), 160, 210, 320);
+
+    //AUDIO
+    //Get the number of samples in a frame
+    uint32_t soundFrameSize = 34100.0f / stella->GameConsole->getFramerate();
+    
+    //Process one frame of audio from stella
+    uint8_t samplebuffer[2048];
+    stella->Sound.processFragment(samplebuffer, soundFrameSize);
+
+    
+	//TODO: setup audio
+	//audio_batch_cb(soundBuffer, BUFFER_LEN);
+    audio_batch_cb(NULL, NULL);
+}
